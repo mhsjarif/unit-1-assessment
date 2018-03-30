5c6129d2d51f8da57450b7e76f9ffe5f91d5a821
Tara Sjarif <tarasjarif@gmail.com>
2018-03-30 13:58:12 -0700

    create js logic

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+
+var addButton = document.getElementById("add-button");
+var reduceButton = document.getElementById("reduce-button");
+var numberDisplay = document.getElementById("number-display");
+var incrementInput = document.getElementById("input-increment");
+
+addButton.addEventListener('click', addFunc);
+reduceButton.addEventListener('click', reduceFunc);
+
+
+var count, increment;
+
+
+initialize();
+function initialize() {
+    count = 0;
+    increment = 0;
+};
+
+function addFunc() {
+    increment =+ parseInt(incrementInput.value, "<br>");
+    count += increment;
+    render();
+};
+
+function reduceFunc() {
+    count -= parseInt(incrementInput.value, "<br>");
+    render();
+};
+
+function render() {
+    numberDisplay.textContent = count;
+
+};
+
+
